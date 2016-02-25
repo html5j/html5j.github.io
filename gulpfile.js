63e369c4d9c19963b797335ac1fb89288af5b3d1
Masataka Yakura <masataka.yakura@gmail.com>
2016-02-26 01:25:10 +0900

    Gulp: BrowserSyncのコードを新しいものに更新

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var minifycss = require('gulp-minify-css')
 
 // サーバのセットアップ
 gulp.task('browser-sync', function () {
-  browserSync({
+  browserSync.init({
     server: {
       baseDir: './',
       directory: true,
@@ -24,13 +24,13 @@ gulp.task('browser-sync', function () {
     open: true,
     notify: false,
   })
-  gulp.watch(['./**/*.html', './css/*.css'], reload)
   gulp.watch('./_src/**/*.css', ['css'])
+  gulp.watch('./**/*.html').on('change', browserSync.reload)
 })
 
 // CSSの生成
 gulp.task('css', function () {
-  gulp.src([
+  return gulp.src([
     'node_modules/normalize.css/normalize.css',
     '_src/css/style.css'
   ])
@@ -42,6 +42,7 @@ gulp.task('css', function () {
   .pipe(uncss({ html: ['index.html'] }))
   .pipe(minifycss({ keepBreaks: true, advanced: false }))
   .pipe(gulp.dest('css'))
+  .pipe(browserSync.stream())
 
   // todo: source maps
 })
